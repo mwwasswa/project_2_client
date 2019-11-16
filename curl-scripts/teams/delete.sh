3dd840fa9af9780b28b6db6ff6d81fb44f03a714
mwasswa <m_wasswa@yahoo.com>
2019-11-16 16:00:32 -0500

    Made slight changes to background
    Fixed background to fit screen size

--- curl-scripts/teams/delete.sh
+++ curl-scripts/teams/delete.sh
@@ -1,4 +1,4 @@
-curl "http://thebadlands.herokuapp.com/teams/${ID}" \
+curl "https://tranquil-journey-54950.herokuapp.com/teams${ID}" \
   --include \
   --request DELETE \
   --header "Authorization: Token token=${TOKEN}" \
